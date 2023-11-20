42e9c113c9d6fa375aa6dd82ac5b8ecf1d169c99
chondosha1234 <jmiller61193@gmail.com>
2023-11-20 15:49:51 -0500

    initial commit

---
+++ src/ass4/Boggle.java
@@ -0,0 +1,54 @@
+package ass4;
+
+public class Boggle {
+
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final double[] FREQUENCIES = {
+            0.08167, 0.01492, 0.02782, 0.04253, 0.12703, 0.02228,
+            0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025,
+            0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987,
+            0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150,
+            0.01974, 0.00074
+    };
+
+    // 16 boggle dice version (4x4 board)
+    private static final String[] BOGGLE_1992 = {
+            "LRYTTE", "VTHRWE", "EGHWNE", "SEOTIS",
+            "ANAEEG", "IDSYTT", "OATTOW", "MTOICU",
+            "AFPKFS", "XLDERI", "HCPOAS", "ENSIEU",
+            "YLDEVR", "ZNRNHL", "NMIQHU", "OBBAOJ"
+    };
+
+
+    // 25 boggle dice version (5x5 board)
+    private static final String[] BOGGLE_BIG = {
+            "AAAFRS", "AAEEEE", "AAFIRS", "ADENNN", "AEEEEM",
+            "AEEGMU", "AEGMNN", "AFIRSY", "BJKQXZ", "CCENST",
+            "CEIILT", "CEILPT", "CEIPST", "DDHNOT", "DHHLOR",
+            "DHLNOR", "DHLNOR", "EIIITT", "EMOTTT", "ENSSSU",
+            "FIPRSY", "GORRVW", "IPRRRY", "NOOTUW", "OOOTTU"
+    };
+
+
+    private String[][] board;
+
+
+    public Boggle(int N) {
+        this.board = new String[N][N];
+
+
+    }
+
+    public Boggle(String[][] board) {
+
+    }
+
+    public Boggle(String[] dice) {
+
+    }
+
+    public Boggle(String filename) {
+
+    }
+
+}
